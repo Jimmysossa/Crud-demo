78ae9a3b901e6a95db24f2a5f57a284fac636054
BrayanBM <98974949+BrayanBM@users.noreply.github.com>
2024-01-14 17:30:24 -0500

    arreglos-css

--- src/components/UseCard.jsx
+++ src/components/UseCard.jsx
@@ -1,3 +1,4 @@
+import './styles/UseCard.css'
 
 const UseCard = ({ user, deleteUser, setUserUpdate, setIsFormClose}) => {
 
@@ -11,17 +12,17 @@ const UseCard = ({ user, deleteUser, setUserUpdate, setIsFormClose}) => {
   }
 
   return (
-    <article>
+    <article className='card-main'>
          <h2>{user.first_name} {user.last_name}</h2>
-         <hr />
+         <hr/>
          <ul>
-            <li><span>Email</span><span>{user.email}</span></li>
-            <li><span>Birthday</span><span>{user.birthday}</span></li>
+            <li className='li-item'><span className='text-span'>Email</span><span>{user.email}</span></li>
+            <li className='li-item'><span className='text-span'>Birthday</span><span>{user.birthday}</span></li>
          </ul>
         <hr />
-        <footer>
-            <button onClick={handleDelete}><i className='bx bx-trash' ></i></button>
-            <button onClick={handleEdit}><i className='bx bx-edit-alt' ></i></button>
+        <footer className='card-buttons'>
+            <button className='btn button-trash' onClick={handleDelete}><i className=' bx bx-trash bx-sm' ></i></button>
+            <button className='btn button-edit'  onClick={handleEdit}><i className=' bx bx-edit-alt bx-sm' ></i></button>
         </footer>
     </article>
   )
